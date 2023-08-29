31ab209d0888dc62ad0f5ddb81bddebb19e42e39
thmrv <thmrvwork@gmail.com>
2023-08-29 12:05:12 +0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
+      <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico"></link>
       <body id="body" className={inter.className}>{children}</body>
     </html>
   )

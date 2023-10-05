798866a182144ca53934831508b46cd4849ba09b
thmrv <thmrvwork@gmail.com>
2023-10-05 15:59:47 +0300

    update

--- src/app/components/tickets.js
+++ src/app/components/tickets.js
@@ -1,35 +1,46 @@
 'use client'
- 
+
 export default function Tickets() {
- 
+
   return (
     <div className="tickets wrapper">
-    <div className="ticket wrapper">
-      <div className="left wrapper">
-        <div className="date">12.10</div>
-        <div className="price">20:00</div>
+      <div className="ticket wrapper">
+        <div className="left wrapper">
+          <div className="date">12.10</div>
+          <div className="price">20:00</div>
+        </div>
+        <div className="middle wrapper">
+          <div className="city">Москва</div>
+          <div className="venue">Крокус Сити Холл</div>
+        </div>
+        <div className="right wrapper">
+          <a href="https://msk.kassir.ru/teatr/crocus-city-hall/sasha-petrov-dramaticheskoe-shou-planeta-maksimus_2023-10-12">Билеты</a>
+        </div>
       </div>
-      <div className="middle wrapper">
-        <div className="city">Москва</div>
-        <div className="venue">Крокус Сити Холл</div>
+      <div className="ticket wrapper">
+        <div className="left wrapper">
+          <div className="date">16.10</div>
+          <div className="price">19:00</div>
+        </div>
+        <div className="middle wrapper">
+          <div className="city">Санкт-Петербург</div>
+          <div className="venue">БКЗ Октрябрьский</div>
+        </div>
+        <div className="right wrapper">
+          <a href="https://spb.kassir.ru/koncert/sasha-petrov-planeta-maksimus">Билеты</a>
+        </div>
       </div>
-      <div className="right wrapper">
-        <a href="https://msk.kassir.ru/teatr/crocus-city-hall/sasha-petrov-dramaticheskoe-shou-planeta-maksimus_2023-10-12">Билеты</a>
+      <div className="ticket wrapper">
+        <div className="left wrapper last-left">
+          <div className="date"><span class="pretext">с</span> 17.10</div>
+        </div>
+        <div className="middle wrapper">
+          <div className="city">Кинотеатры России</div>
+        </div>
+        <div className="right wrapper">
+          <a href="https://www.afisha.ru/movie/planeta-maksimus-295258/">Билеты</a>
+        </div>
       </div>
     </div>
-    <div className="ticket wrapper">
-      <div className="left wrapper">
-        <div className="date">16.10</div>
-        <div className="price">19:00</div>
-      </div>
-      <div className="middle wrapper">
-        <div className="city">Санкт-Петербург</div>
-        <div className="venue">БКЗ Октрябрьский</div>
-      </div>
-      <div className="right wrapper">
-        <a href="https://spb.kassir.ru/koncert/sasha-petrov-planeta-maksimus">Билеты</a>
-      </div>
-    </div>
-  </div>
   )
 }
